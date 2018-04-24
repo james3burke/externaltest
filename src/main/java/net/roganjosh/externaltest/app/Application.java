e70ce93dda14325d56e47fb5fdb5c05dbb0e1b98
James Burke <jim@roganjosh.net>
2018-04-24 16:10:03 +0100

    initial

---
+++ src/main/java/net/roganjosh/externaltest/app/Application.java
@@ -0,0 +1,22 @@
+package net.roganjosh.externaltest.app;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ *
+ */
+@SpringBootApplication
+@ComponentScan("net.roganjosh.externaltest")
+public class Application {
+
+    private final static Logger logger = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
